d30df58f1c12f6665e0c0ad693a51e4c2043ba39
Heavy Bob <ferrettclay@gmail.com>
2023-05-10 11:50:41 +1000

    Added all tiers for weapon_script.

--- src/weapons_script.js
+++ src/weapons_script.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const xml2js = require('xml2js');
 
 const parser = new xml2js.Parser();
 const fileName = 'ModuleData/Markets/weaponmarketall.xml';
 
-const parseTier1Items = (tier1ItemsString) => {
-  const items = tier1ItemsString.split('|');
+function parseTierItems(tierItemsString) {
+  const items = tierItemsString.split('|');
   const parsedItems = items.map((item) => {
     const [id, sell_price, buy_price] = item.split('*');
     return {
@@ -15,29 +15,42 @@ const parseTier1Items = (tier1ItemsString) => {
     };
   });
   return parsedItems;
-};
+}
 
-const saveJsonFile = async (filePath, data) => {
+async function saveJsonFile(filePath, data) {
   try {
-    await fs.promises.writeFile(filePath, JSON.stringify(data, null));
+    await fs.writeFile(filePath, JSON.stringify(data, null));
     console.log(`Data saved to ${filePath}`);
   } catch (error) {
     console.error('Error writing JSON file:', error);
   }
-};
+}
 
-const readXMLFile = async (inputFilePath, outputFilePath) => {
+async function readXMLFile(inputFilePath, outputFilePath) {
   try {
-    const xmlData = await fs.promises.readFile(inputFilePath, 'utf8');
+    const xmlData = await fs.readFile(inputFilePath, 'utf8');
     const parsedData = await parser.parseStringPromise(xmlData);
-    const tier1Items = parseTier1Items(parsedData.Market.Tier1Items[0]);
 
-    await saveJsonFile(outputFilePath, tier1Items);
+    const marketData = parsedData.Market;
+    const tier1Items = parseTierItems(marketData.Tier1Items[0]);
+    const tier2Items = parseTierItems(marketData.Tier2Items[0]);
+    const tier3Items = parseTierItems(marketData.Tier3Items[0]);
+    const tier4Items = parseTierItems(marketData.Tier4Items[0]);
+
+    const allItems = {
+      Tier1: tier1Items,
+      Tier2: tier2Items,
+      Tier3: tier3Items,
+      Tier4: tier4Items,
+    };
+
+    await saveJsonFile(outputFilePath, allItems);
   } catch (error) {
     console.error('Error reading or parsing XML file:', error);
   }
-};
+}
 
 const inputFilePath = fileName;
 const outputFilePath = 'gen_json_debug/weapons.json';
+
 readXMLFile(inputFilePath, outputFilePath);
